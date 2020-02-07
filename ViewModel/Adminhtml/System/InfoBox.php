436a90429e12f66e27580e2016de18d48686466f
Gurjit GS. Singh <gurjit.singh@netresearch.de>
2020-02-07 13:02:06 +0000

    PDTWO-31: add module configuration and info box

---
+++ ViewModel/Adminhtml/System/InfoBox.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace PostDirekt\Addressfactory\ViewModel\Adminhtml\System;
+
+use PostDirekt\Addressfactory\Model\Config;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+/**
+ * Class InfoBox
+ *
+ * @author  Gurjit Singh <gurjit.singh@netresearch.de>
+ * @link    https://www.netresearch.de/
+ */
+class InfoBox implements ArgumentInterface
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * InfoBox constructor.
+     *
+     * @param Config $coreConfig
+     */
+    public function __construct(Config $coreConfig)
+    {
+        $this->config = $coreConfig;
+    }
+
+    /**
+     * Obtain the Module Version from Config.
+     *
+     * @return string
+     */
+    public function getModuleVersion(): string
+    {
+        return $this->config->getModuleVersion();
+    }
+}
