7b5726cbd9c8e493897a51c0a7bbc790a93b5db8
Gurjit GS. Singh <gurjit.singh@netresearch.de>
2020-02-19 15:19:16 +0000

    PDTWO-33: Show Analysis Data on Order Detail Page

---
+++ ViewModel/Adminhtml/AnalysisData.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace PostDirekt\Addressfactory\ViewModel\Adminhtml;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use PostDirekt\Addressfactory\Model\AnalysisResult;
+use PostDirekt\Addressfactory\Model\AnalysisResultRepository;
+use Magento\Framework\View\Asset\Repository as AssetRepository;
+
+/**
+ * AnalysisData
+ *
+ * @author  Gurjit Singh <gurjit.singh@netresearch.de>
+ * @link    https://www.netresearch.de/
+ */
+class AnalysisData implements ArgumentInterface
+{
+    /**
+     * @var AnalysisResultRepository
+     */
+    private $analysisResultRepository;
+
+    /**
+     * @var AssetRepository
+     */
+    private $assetRepository;
+
+    public function __construct(
+        AnalysisResultRepository $analysisResultRepository,
+        AssetRepository $assetRepository
+    ) {
+        $this->analysisResultRepository = $analysisResultRepository;
+        $this->assetRepository = $assetRepository;
+    }
+
+    public function getAnalysisResult(int $addressId): ?AnalysisResult
+    {
+        try {
+            return $this->analysisResultRepository->getByAddressId((string) $addressId);
+        } catch (NoSuchEntityException $exception) {
+            return null;
+        }
+    }
+
+    public function getLogoUrl(): string
+    {
+        return $this->assetRepository->getUrl('PostDirekt_Addressfactory::images/logo_addressfactory.png');
+    }
+}
