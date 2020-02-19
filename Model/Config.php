f85896271a3b75c738383ed69245e5a198d6cafd
Sebastian Ertner <sebastian.ertner@netresearch.de>
2020-02-19 17:48:10 +0000

    PDTWO-37: Extend Config with Order processing options

--- Model/Config.php
+++ Model/Config.php
@@ -9,54 +9,36 @@ namespace PostDirekt\Addressfactory\Model;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
-/**
- * Class Config
- *
- * @author   Gurjit Singh <gurjit.singh@netresearch.de>
- * @link     https://www.netresearch.de/
- */
 class Config
 {
     private const CONFIG_PATH_VERSION = 'postdirekt/addressfactory/version';
-    private const CONFIG_PATH_MANDATENAME = 'postdirekt/addressfactory/mandatename';
+    private const CONFIG_PATH_MANDATENAME = 'postdirekt/addressfactory/mandate_name';
     private const CONFIG_PATH_LOGGING = 'postdirekt/addressfactory/logging';
-    private const CONFIG_PATH_LOGLEVEL = 'postdirekt/addressfactory/loglevel';
-    private const CONFIG_PATH_SANDBOXMODE = 'postdirekt/addressfactory/sandboxmode';
-    private const CONFIG_PATH_ADJUSTMENTSTRENGTH = 'postdirekt/addressfactory/adjustmentstrength';
-    private const CONFIG_PATH_CONFIGURATIONNAME = 'postdirekt/addressfactory/configurationname';
+    private const CONFIG_PATH_LOGLEVEL = 'postdirekt/addressfactory/log_level';
+    private const CONFIG_PATH_SANDBOXMODE = 'postdirekt/addressfactory/sandbox_mode';
+    private const CONFIG_PATH_ADJUSTMENTSTRENGTH = 'postdirekt/addressfactory/adjustment_strength';
+    private const CONFIG_PATH_CONFIGURATIONNAME = 'postdirekt/addressfactory/configuration_name';
+
+    private const CONFIG_PATH_HOLD_NON_DELIVERABLE_ORDERS = 'postdirekt/addressfactory/hold_non_deliverable_orders';
+    private const CONFIG_PATH_HOLD_AUTO_CANCEL_ORDERS = 'postdirekt/addressfactory/auto_cancel_orders';
+    private const CONFIG_PATH_HOLD_AUTO_UPDATE_SHIPPING_ADDRESS = 'postdirekt/addressfactory/auto_update_shipping_address';
 
     /**
      * @var ScopeConfigInterface
      */
     private $scopeConfig;
 
-    /**
-     * Config constructor.
-     * @param ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(
-        ScopeConfigInterface $scopeConfig
-    ) {
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
         $this->scopeConfig = $scopeConfig;
     }
 
-    /**
-     * Obtain the module version from config.
-     *
-     * @return string
-     */
     public function getModuleVersion(): string
     {
         return $this->scopeConfig->getValue(self::CONFIG_PATH_VERSION);
     }
 
-    /**
-     * Obtain logging enabled setting from config.
-     *
-     * @param null $store
-     * @return bool
-     */
-    public function isLoggingEnabled($store = null): bool
+    public function isLoggingEnabled(?string $store = null): bool
     {
         return (bool) $this->scopeConfig->getValue(
             self::CONFIG_PATH_LOGGING,
@@ -65,13 +47,7 @@ class Config
         );
     }
 
-    /**
-     * Obtain log level from config.
-     *
-     * @param null $store
-     * @return string
-     */
-    public function getLogLevel($store = null): string
+    public function getLogLevel(?string $store = null): string
     {
         return (string) $this->scopeConfig->getValue(
             self::CONFIG_PATH_LOGLEVEL,
@@ -80,28 +56,16 @@ class Config
         );
     }
 
-    /**
-     * Obtain sandbox mode setting from config.
-     *
-     * @param null $store
-     * @return string
-     */
-    public function getSandboxMode($store = null): string
+    public function isSandboxMode(?string $store = null): bool
     {
-        return (string) $this->scopeConfig->getValue(
+        return (bool) $this->scopeConfig->getValue(
             self::CONFIG_PATH_SANDBOXMODE,
             ScopeInterface::SCOPE_STORE,
             $store
         );
     }
 
-    /**
-     * Obtain configuration name.
-     *
-     * @param null $store
-     * @return string
-     */
-    public function getConfigurationName($store = null): string
+    public function getConfigurationName(?string $store = null): string
     {
         return (string) $this->scopeConfig->getValue(
             self::CONFIG_PATH_CONFIGURATIONNAME,
@@ -110,13 +74,7 @@ class Config
         );
     }
 
-    /**
-     * Obtain adjustment strength from config.
-     *
-     * @param null $store
-     * @return string
-     */
-    public function getAdjustmentStrength($store = null): string
+    public function getAdjustmentStrength(?string $store = null): string
     {
         return (string) $this->scopeConfig->getValue(
             self::CONFIG_PATH_ADJUSTMENTSTRENGTH,
@@ -125,16 +83,37 @@ class Config
         );
     }
 
-    /**
-     * Obtain mandate name from config.
-     *
-     * @param null $store
-     * @return string
-     */
-    public function getMandateName($store = null): string
+    public function getMandateName(?string $store = null): string
     {
         return (string) $this->scopeConfig->getValue(
             self::CONFIG_PATH_MANDATENAME,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    public function isHoldNonDeliverableOrders(?string $store = null): bool
+    {
+        return (bool) $this->scopeConfig->getValue(
+            self::CONFIG_PATH_HOLD_NON_DELIVERABLE_ORDERS,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    public function isAutoCancelNonDeliverableOrders(?string $store = null): bool
+    {
+        return (bool) $this->scopeConfig->getValue(
+            self::CONFIG_PATH_HOLD_AUTO_CANCEL_ORDERS,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    public function isAutoUpdateShippingAddress(?string $store = null): bool
+    {
+        return (bool) $this->scopeConfig->getValue(
+            self::CONFIG_PATH_HOLD_AUTO_UPDATE_SHIPPING_ADDRESS,
             ScopeInterface::SCOPE_STORE,
             $store
         );
